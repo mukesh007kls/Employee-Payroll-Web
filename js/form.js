8c6a2368e8415ce54a30a4a2547877a319f95ed0
mukesh007kls <mallikanti2000@gmail.com>
2023-04-15 00:13:19 +0530

    ADDED:Code to implement eventlistener on salary

--- js/form.js
+++ js/form.js
@@ -1,21 +1,5 @@
-window.addEventListener('DOMContentLoaded',(event)=>{
-    validName();
-    salaryRange();
-})
-function validName(){
-    const name=document.querySelector('#name');
-    const textError=document.querySelector('.text-error');
-    name.addEventListener('input',function(){
-        if(name.ariaValueMax.length==0){
-            textError.textContent="";
-            return;
-        }
-        try{
-            let empData=new EmployeePayrollData();
-            empData.name=name.value;
-            textError.textContent="";
-        }catch(e){
-            textError.textContent=e;
-        }
-    });
-}
+const salaryValue=document.querySelector('.salary-output');
+const salaryInputRange=document.querySelector('#salary');
+salaryInputRange.addEventListener("input",(event)=>{
+    salaryValue.textContent=salaryInputRange.value;
+});

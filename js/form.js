3e3f7540da93df58b691f2ddaf0c1584b5743812
mukesh007kls <mallikanti2000@gmail.com>
2023-04-15 22:27:27 +0530

    ADDED:Code to store data in local storage

--- js/form.js
+++ js/form.js
@@ -9,8 +9,10 @@ window.addEventListener('DOMContentLoaded', (event) =>{
        try{
            (new EmployeePayRollData()).name = name.value;
            textError.textContent = "";
+           name.computedStyleMap.border='2px solid green';
        }catch(e){
            textError.textContent = e;
+           name.computedStyleMap.border='2px solid red';
        }
     });
     
@@ -28,32 +30,48 @@ const save = () => {
     let employeePayrollData;
     try{
         employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     }catch(e){
         return;
     }
 }
 
-
-const salaryValue=document.querySelector('.salary-output');
-const salaryInputRange=document.querySelector('#salary');
-
-salaryInputRange.addEventListener("input",(event)=>{
-    salaryValue.textContent=salaryInputRange.value;
-});
-
-function createEmployeePayroll() {
+const createEmployeePayroll=()=> {
     let employeePayrollData = new EmployeePayrollData();
-    employeePayrollData.name = getInputValue("name");
+    try{
+        employeePayrollData.name=getInputValue('#name');
+    }catch(e){
+        setTextValue('.text-error',e);
+        throw e;
+    }
     employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
     employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
     employeePayrollData.department = getSelectedValues("[name = department]");
     employeePayrollData.salary = getInputValue("salary");
     employeePayrollData.notes = getInputValue("notes");
-    employeePayrollData.startDate = new Date(getInputValue("year") + getInputValue("month") + getInputValue("day"));
+    let year=getInputValue('#year');
+    let month=parseInt(getInputValue('#month'));
+    let day=getInputValue('#day');
+    let date=day+" "+month+" "+year;
+    employeePayrollData.startDate = Date.parse(date);
     alert(employeePayrollData.toString());
-    
+    return employeePayrollData;
 }
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePeyrollList"));
     
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(EmployeePayrollData);
+    }
+    else{
+        employeePayrollList = [employeePayrollData];
+    }
+    
+    alert(employeePayrollList.toString());
+     localStorage.setItem("EmployeePeyrollList",JSON.stringify(employeePayrollList)); 
+}
+
 function getInputValue(id) {
     let value = document.getElementById(id).value;
     return value;

3001d963b8f8637ba8553e1a00161bc01bff08de
mukesh007kls <mallikanti2000@gmail.com>
2023-04-15 18:53:59 +0530

    ADDED:Event listener on salary and validated date

--- js/form.js
+++ js/form.js
@@ -1,3 +1,38 @@
+window.addEventListener('DOMContentLoaded', (event) =>{
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function() {
+       if(name.value.length == 0){
+           textError.textContent = "";
+           return;
+       } 
+       try{
+           (new EmployeePayRollData()).name = name.value;
+           textError.textContent = "";
+       }catch(e){
+           textError.textContent = e;
+       }
+    });
+    
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function () {
+        output.textContent = salary.value; 
+    });
+    
+});
+    
+    
+const save = () => {
+    let employeePayrollData;
+    try{
+        employeePayrollData = createEmployeePayroll();
+    }catch(e){
+        return;
+    }
+}
+
 
 const salaryValue=document.querySelector('.salary-output');
 const salaryInputRange=document.querySelector('#salary');
@@ -6,7 +41,7 @@ salaryInputRange.addEventListener("input",(event)=>{
     salaryValue.textContent=salaryInputRange.value;
 });
 
-function createObject() {
+function createEmployeePayroll() {
     let employeePayrollData = new EmployeePayrollData();
     employeePayrollData.name = getInputValue("name");
     employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
@@ -24,8 +59,8 @@ function getInputValue(id) {
     return value;
 }
     
-function getSelectedValues(property) {
-    let allItems = document.querySelectorAll(property);
+function getSelectedValues(propertyValue) {
+    let allItems = document.querySelectorAll(propertyValue);
     let selectedItems = [];
     allItems.forEach(item => {
         if (item.checked)
